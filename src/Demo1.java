e7a98dd45d863b041c1b86f1f09ec41ebe97f0dc
Balance <1835675603@qq.com>
2023-09-03 16:19:56 +0800

    changes

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,11 +1,23 @@
 import java.util.Arrays;
 
 public class Demo1 {
-
     public static void main(String[] args) {
-
+        Test test = new Test(){
+            @Override
+            public void say() {
+                System.out.println("lalala");
+            }
+        };
+        test.say();
 
     }
+}
+class Test{
+    public Test() {
+    }
 
+    public void say(){
+        System.out.println("hhhh");
+    }
 
 }
---
+++ src/JavaCommonMethod.md
@@ -0,0 +1,7 @@
+## 类
+```java
+对象名.getclass();//返回此Object的运行时类
+```
+```java
+public String toString();//对象的字符串表示形式。
+```
